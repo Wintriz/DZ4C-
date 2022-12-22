07265930ac6f2269b78b301530c985eb562953ac
Andrey <rf5800@mail.ru>
2022-12-22 09:25:16 +0300

    Zad27

--- DZ4_Zad27/Program.cs
+++ DZ4_Zad27/Program.cs
@@ -5,3 +5,11 @@
 
 Console.WriteLine("Введите число");
 int num = int.Parse(Console.ReadLine()!);
+int result = 0;
+while (num > 0)
+{
+    result = result + num % 10;
+    num = num / 10;
+}
+Console.WriteLine(result);
+
